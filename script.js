07d43be50b34475ed086b1f8021555490aa7cf45
Natalia Namie <namiekomesso@gmail.com>
2023-05-26 18:21:31 -0300

    resolução dos exercicios

---
+++ script.js
@@ -0,0 +1,17 @@
+const btnLogin = document.getElementById('btn-login');
+
+btnLogin.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  const emailInput = document.querySelector('input[name="email"]');
+  const passwordInput = document.querySelector('input[name="password"]');
+
+  const email = emailInput.value;
+  const password = passwordInput.value;
+
+  if (email === 'tryber@teste.com' && password === '123456') {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Email ou senha inválidos.');
+  }
+});
